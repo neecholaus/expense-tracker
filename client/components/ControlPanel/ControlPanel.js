25f9709b0f6c2efb1de4c2bc984bd3ec86688010
neecholaus <thisisnickadam@gmail.com>
2019-04-03 21:19:07 -0700

    First two digits passed as amount will be evaluated as dollar amount.

--- client/components/ControlPanel/ControlPanel.js
+++ client/components/ControlPanel/ControlPanel.js
@@ -73,11 +73,16 @@ class ControlPanel extends Component {
 
     /**
      *  Returns an integer formatted as US cents.
-     *  i.e. $45.20 => 4520
+     *  i.e. $45.20 => 4520, 30 => $30.00, 020 => $0.20
      *  @param string
      */
     amountToInt = amount => {
-        return parseInt(amount.replace(/\D/g, ''));
+        let split = [...amount];
+
+        // Ensure initial 2 digits are evaluated as dollars instead of cents
+        while(split.length < 3) split.push('0');
+
+        return parseInt(split.join('').replace(/\D/g, ''));
     }
 
     render() {
