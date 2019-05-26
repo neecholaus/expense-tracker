ba3434f9c5a9499b89327e8708160762624ad575
neecholaus <thisisnickadam@gmail.com>
2019-05-25 22:25:28 -0700

    Fixed index routing.

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ let app = express();
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get(['/', '/*'], (req, res) => {
+app.get('/', (req, res) => {
+    res.redirect('/control');
+});
+
+app.get('/*', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
