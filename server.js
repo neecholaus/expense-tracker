f4ce36cbd0d88c1a6350fcca0e52864f43044330
neecholaus <thisisnickadam@gmail.com>
2019-05-25 22:20:17 -0700

    Updated server.js for dynamic port.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+const PORT = process.env.PORT;
+
 let app = express();
 
 app.use('/public', express.static(__dirname + '/public'));
@@ -9,4 +11,4 @@ app.get(['/', '/*'], (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000);
+app.listen(PORT || 3000);
